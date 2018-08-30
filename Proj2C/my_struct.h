e32d1cf0491587d8a1ce6c3901093157de519191
JakeBeder <34498861+JakeBeder@users.noreply.github.com>
2018-08-30 14:42:53 -0700

    Project 2C - struct practice

---
+++ Proj2C/my_struct.h
@@ -0,0 +1,22 @@
+
+
+typedef struct {
+	double minX;
+	double maxX;	
+	double minY;	
+	double maxY;	
+} Rectangle;
+
+typedef struct {
+	double originX;
+	double originY;
+	double radius;
+} Circle;
+
+typedef struct {
+
+	double pt1X;
+	double pt2X;
+	double minY;
+	double maxY;
+} Triangle;
